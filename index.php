7a7e65776e89498e882aa1d24c0413abd6a29d91
WILLIAMS AMAYA <whistemalo@gmail.com>
2022-02-02 22:49:57 -0600

    api rest

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
     <title>Cambio de Divisa</title>
 </head>
 <body>
-    
+    <?php
+
+        $headers = [
+            "User-Agent: Example REST API Client"  
+        ];
+
+        $ch = curl_init("https://api.github.com/user/repos");
+        
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_exec($ch);
+
+        curl_close($ch);
+    ?>
 </body>
 </html>
